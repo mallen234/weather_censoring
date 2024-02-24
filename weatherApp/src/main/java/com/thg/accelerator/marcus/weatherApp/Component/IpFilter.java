cc02d7207a560695881bca693cad39b09c2109a6
Marcus Allen <marcus.allen84@thehutgroup.com>
2024-02-24 23:45:01 +0000

    checking why API doesn't work on remote by removing ip filter :)

--- weatherApp/src/main/java/com/thg/accelerator/marcus/weatherApp/Component/IpFilter.java
+++ weatherApp/src/main/java/com/thg/accelerator/marcus/weatherApp/Component/IpFilter.java
@@ -8,35 +8,35 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-
-@Component
-public class IpFilter implements Filter {
-
-    @Value("${allowed.ips}")
-    private String allowedIps;
-
-    private List<String> allowedIpList;
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        allowedIpList = Arrays.asList(allowedIps.split(","));
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        String clientIp = request.getRemoteAddr();
-        System.out.println(clientIp);
-        if (allowedIpList.contains(clientIp)) {
-            chain.doFilter(request, response);
-        } else {
-            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-            httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        }
-    }
-
-    @Override
-    public void destroy() {
-    }
-}
+//
+//@Component
+//public class IpFilter implements Filter {
+//
+//    @Value("${allowed.ips}")
+//    private String allowedIps;
+//
+//    private List<String> allowedIpList;
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        allowedIpList = Arrays.asList(allowedIps.split(","));
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//        String clientIp = request.getRemoteAddr();
+//        System.out.println(clientIp);
+//        if (allowedIpList.contains(clientIp)) {
+//            chain.doFilter(request, response);
+//        } else {
+//            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+//            httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+//        }
+//    }
+//
+//    @Override
+//    public void destroy() {
+//    }
+//}
 
